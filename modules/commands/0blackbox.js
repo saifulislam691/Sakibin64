6cde95d0ecb1de53ac366bc9065885587871a1f5
Sakibin <91206317+Sakibin@users.noreply.github.com>
2024-10-11 01:25:11 +0600

    Update 0blackbox.js

--- modules/commands/0blackbox.js
+++ modules/commands/0blackbox.js
@@ -1,9 +1,10 @@
 const axios = require("axios");
 const APIKEY = "SAKI-BIN-SWT56X";
+
 module.exports.config = {
     name: "black",
     version: "0.4.1",
-    hasPermission: 0,
+    hasPermssion: 0,
     credits: "Sakibin",
     description: "Blackbox A.i",
     usages: "(your question?)",
@@ -13,16 +14,20 @@ module.exports.config = {
 
 module.exports.run = async ({ api, event, args }) => {
     try {
-     let prompt = args.join("");
+        let prompt = args.join("");
         if (!prompt) {
             return api.sendMessage(`➤ Hi, I'm MR. Black from Sakibin D-Base..🎩`, event.threadID, event.messageID);
         }
 
         const response = await axios.get(`https://xakibin.onrender.com/api/blackbox?prompt=${prompt}&apikey=SAKIBIN-FREE-SY6B4X`);
-        const respond = response.data.message;
-        api.sendMessage(respond, event.threadID, event.messageID);
+        const { message: result } = response.data;
+
+        const responseMessage = `𝗖𝗛𝗔𝗧𝗚𝗣𝗧\n━━━━━━━━━━━━━━━━━━\n${result}\n━━━━━━━━━━━━━━━━━━`;
+        api.editMessage(responseMessage, event.threadID, event.messageID);
     } catch (error) {
         console.error("An error occurred:", error);
-        api.sendMessage("Oops! Something went wrong.", event.threadID, event.messageID);
+
+        const errorMessage = `𝗖𝗛𝗔𝗧𝗚𝗣𝗧\n━━━━━━━━━━━━━━━━━━\nOops! Something went wrong.\n━━━━━━━━━━━━━━━━━━`;
+        api.editMessage(errorMessage, event.threadID, event.messageID);
     }
 };
